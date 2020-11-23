5d3d9d46d0e517c0229427f99d14ea5cdcdde246
SoorajModi <soomodi@gmail.com>
2020-11-22 22:36:18 -0500

    Add fixes for deployment

--- app.js
+++ app.js
@@ -71,6 +71,11 @@ app.get("/posts/:post", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port === "") {
+  port = 8300;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port " + port);
 });
